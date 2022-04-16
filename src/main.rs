8f2ca8a91896c7e555bbd12fcb55d33e2fd7ff1d
AQadir64 <a.qadir67@outlook.com>
2022-04-16 17:02:16 +0500

    borrowing and ownership practice

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use std::{collections::HashMap, fs::read_to_string};
 fn main() {
     let greetings = "Hello World!";
     println!("{}", greetings);
+
     greet("mars");
+
     strings();
-    Assignments();
+
+    assignments();
+
+    temp();
+
+    borrowing();
+
+    everthing_return_something();
+
+    add_numbers(5, 6);
 }
 
 fn greet(target: &str) {
@@ -36,6 +48,13 @@ fn strings() {
 
     greet(going_back_str_two);
     greet(going_back_str);
+
+    print_type_of(&"Hi!");
+    print_type_of(&String::new());
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("Type of {}", std::any::type_name::<T>());
 }
 
 // &str is the primitive type, an immutable reference to the String
@@ -50,9 +69,11 @@ fn strings() {
 // &string method can be used anywhere. It automatically turns into the &str
 // if the method needs it.
 
+// &str are the pointer to the substring in other string data
+
 /* --------------------------------- Variable Assignments & Mutability ----------------------------------------- */
 
-fn Assignments() {
+fn assignments() {
     let mut mutable = 1;
 
     println!("{}", mutable);
@@ -73,3 +94,72 @@ fn Assignments() {
 // re-assignment value to a variable must of the same time.
 
 // we can assign a different type to a variable by re declaring it using the let keyword
+
+fn temp() {
+    let source = read_to_string("./README.md").unwrap();
+
+    let mut files = HashMap::new();
+
+    files.insert("README", source.clone()); // secure the ownership of the variable
+
+    files.insert("READMEW2", source); // copy traits issue if we pass source again here
+                                      // should be send as copy in a first place
+}
+
+fn borrowing() {
+    let source = read_to_string("./README.md").unwrap();
+
+    let mut files = HashMap::new();
+
+    files.insert("README", source.clone());
+
+    files.insert("README2", source);
+
+    let files_ref = &files;
+
+    print_borrowed_files(files_ref);
+
+    let files_ref2 = &mut files;
+
+    print_borrowed_files(files_ref2);
+}
+
+fn print_borrowed_files(map: &HashMap<&str, String>) {
+    println!("{:?}", map);
+}
+
+// :? in the println! is for the debugging format. It is used to ouput the no human
+// in the readable form
+
+// to use the mutable reference first reference must end before the second
+
+/* --------------------------------- Style Guides ----------------------------------------- */
+
+// varibales functions and modules are in snake case
+// constants are in capital camel case
+// contants are in Pascal case
+
+// if ( x > y) {} can be written as if x > y { } // this style is prefered
+
+fn everthing_return_something() {
+    let apples = 6;
+
+    let message = if apples == 6 {
+        "pile of apple"
+    } else if apples > 6 {
+        "more then a pile"
+    } else {
+        "less then a pile"
+    };
+
+    println!("{}", message);
+}
+
+// what if we add ; it will return the () that is called unit type
+// no concept of null and undefined in the rust
+
+// implicit return types
+
+fn add_numbers(left: i64, right: i64) -> i64 {
+    return left + right;
+}
