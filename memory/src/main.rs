1fdf38c7b3bd0c1976b4682bf8299f3a60758a12
AQadir64 <a.qadir67@outlook.com>
2022-04-22 17:46:40 +0500

    memory management with smart pointer

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,28 +1,23 @@
-use std::{slice::from_raw_parts, str::from_utf8_unchecked};
 
 fn main() {
-    println!("Hello, world!");
-
-    let (pointer, length) = get_memory_location();
-
-    let message = get_str_at_location(pointer, length);
-
-    println!(
-        "the bytes {} at 0x{:X} stored : {}",
-        length, pointer, message
-    );
+    let a = 2;
+    let result = stack_only(a);
+    println!("{}", result);
+    // println! is a macro 
+    // a macro is a code used to write more code
+    
 }
 
-fn get_memory_location() -> (usize, usize) {
-    let string = "beluga";
-
-    let pointer = string.as_ptr() as usize;
-
-    let length = string.len();
-
-    (pointer, length)
+fn stack_only(b: i32) -> i32 {
+    let c = 3;
+    return b + c + stack_and_heap(); 
 }
 
-fn get_str_at_location(pointer: usize, length: usize) -> &'static str {
-    unsafe { from_utf8_unchecked(from_raw_parts(pointer as *const u8, length)) }
+fn stack_and_heap() -> i32 {
+    let d = 5;
+    let e = Box::new(7);
+    // smart pointer that does'nt need to be reallocatable once goes out of scope
+    // Box::new() points to the location of the value in heap
+    return d+ *e;
 }
+
